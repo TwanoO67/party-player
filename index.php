15ecf9156b46a158c15be1c84cdbbd0dafddebfb
Antoine WEBER <twanoo@rdl-pc.home>
2015-01-01 12:27:59 +0100

    gestion de fermeture des bootbox bugfix

--- index.php
+++ index.php
@@ -166,7 +166,7 @@
 			
 			Intro.on("hide", function() {    // remove the event listeners when the dialog is dismissed
 		        bootbox.alert("Vous devez choisir de participer à une playlist, ou d'en creer une nouvelle...",function(){
-			        window.reload();
+			        document.location.reload();
 		        });
 		        
 		    });
