91ce0af2876b8281c79f2a6bdbad0fac8b80d4c6
Antoine WEBER <a.weber@sdv.fr>
2015-02-06 17:43:03 +0100

    ajout du gitignore

--- server.php
+++ server.php
@@ -379,7 +379,25 @@
                 include './includes/spotify-web-api/SpotifyWebAPIException.php';
 
                 
-                $this->request = new SpotifyWebAPI\Request();
+                $request = new SpotifyWebAPI\Request();
+ 
+                $session = new SpotifyWebAPI\Session(getenv('SPOTIFY_CLIENT_ID'), getenv('SPOTIFY_CLIENT_SECRET'), getenv('SPOTIFY_REDIRECT_URI'));
+                $api = new SpotifyWebAPI\SpotifyWebAPI();
+                
+                if (isset($_GET['code'])) {
+                    $session->requestToken($_GET['code']);
+                    $api->setAccessToken($session->getAccessToken());
+                
+                    print_r($api->me());
+                    
+                    $response = $api->getUserPlaylistTracks('mcgurk', $this->playlistID);
+                } else {
+                    header('Location: ' . $session->getAuthorizeUrl(array(
+                        'scope' => array('user-read-email', 'user-library-modify')
+                    )));
+                }
+
+                
                 	                
                 if($id > 0){
                     $reponse['result'] = 'success';
