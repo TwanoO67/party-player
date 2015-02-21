b4becdfb36227d818dd6d69e6cddb396477b0ce9
Antoine WEBER <a.weber@sdv.fr>
2015-02-21 11:21:00 +0100

    no feature loop

--- test2.php
+++ test2.php
@@ -11,11 +11,6 @@
 
 	<meta charset="UTF-8" />
 	<meta http-equiv="content-language" content="en" />
-	<title>MediaElement.js - HTML5 video player and audio player with Flash and Silverlight shims</title>
-
-	<meta name="Copyright" content="Copyright (c) John Dyer" />
-	<meta name="dc.language" content="en" />
-	<meta name="geo.country" content="US" />
 
 	<!-- mediaelement -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script> 
@@ -31,39 +26,6 @@
 <body>
 	<div id="container">
 
-		<div id="top">
-			<div id="header">
-				<div id="logo">
-					<h1><a href="/">MediaElement.js</a></h1>
-					<!-- <p>HTML5 &lt;video&gt; and &lt;audio&gt; with H.264, FLV, WMV, MP3 on any browser.</p> -->
-				</div>
-				<div id="nav">
-					<ul>
-						<li><a href="/">Home</a></li>
-						<li><a href="http://blog.mediaelementjs.com/">Blog</a></li>
-						<li><a href="/#howitworks">How it Works</a></li>
-						<li><a href="/#plugins">Plugins</a></li>
-						<li><a href="/#installation">Installation</a></li>
-						<li><a href="/#api">Options</a></li>
-						<li><a href="/support/">Support</a></li>
-						<!--<li><a href="#contact">Contact</a></li>-->
-					</ul>
-				</div>
-			</div>
-		</div>
-
-
-
-
-
-<script src="/js/mep-feature-loop.js"></script>
-
-<div class="container_12" class="plugin-header">
-	<div class="grid_12">
-		<h2>YouTube API Example</h2>
-	</div>
-</div>
-<div class="clear"></div>
 
 	
 <div class="plugin-example-container">
