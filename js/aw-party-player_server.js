1473002fdbc6ab84a67f051fc94e22409e4d781a
Antoine WEBER <a.weber@sdv.fr>
2015-02-21 10:49:23 +0100

    juste declaration de l'objet

--- js/aw-party-player_server.js
+++ js/aw-party-player_server.js
@@ -315,75 +315,59 @@ function load(url){
         htmlin = '<audio id="audio-player" autoplay="true" preload="none" src="'+url+'" type="audio/mp3" controls="controls"/></audio>';
     }
     
-   //if(mediaPlayer.pluginType == 'flash'){
-        jQuery('#player-wrapper').html(htmlin);
-        /*mediaPlayer = new MediaElementPlayer("#audio-player",
+    jQuery('#player-wrapper').html(htmlin);
+    
+    mediaPlayer = new MediaElementPlayer("#audio-player");
+        
+    /*mediaPlayer = new MediaElementPlayer("#audio-player",{
+        // if set, overrides <video width>
+        videoWidth: -1,
+        // if set, overrides <video height>
+        videoHeight: -1,
+        // width of audio player
+        audioWidth: '100%',
+        // height of audio player
+        audioHeight: 30,
+        // initial volume when the player starts
+        startVolume: 1,
+        // useful for <audio> player loops
+        loop: false,
+        // enables Flash and Silverlight to resize to content size
+        enableAutosize: true,
+        // the order of controls you want on the control bar (and other plugins below)
+        features: ['playpause','progress','current','duration','tracks','volume','fullscreen'],
+        // Hide controls when playing and mouse is not over the video
+        alwaysShowControls: false,
+        // force iPad's native controls
+        iPadUseNativeControls: false,
+        // force iPhone's native controls
+        iPhoneUseNativeControls: false, 
+        // force Android's native controls
+        AndroidUseNativeControls: false,
+        // forces the hour marker (##:00:00)
+        alwaysShowHours: false,
+        // show framecount in timecode (##:00:00:00)
+        showTimecodeFrameCount: false,
+        // used when showTimecodeFrameCount is set to true
+        framesPerSecond: 25,
+        // turns keyboard support on and off for this instance
+        enableKeyboard: true,
+        // when this player starts, it will pause other players
+        pauseOtherPlayers: true,
+        // array of keyboard commands
+        keyActions: [],
+        success: function(mediaElement, domObject)
         {
-           alwaysShowControls: true,
-           features: ['playpause','volume','progress','fullscreen'],
-           audioVolume: 'horizontal',
-           audioWidth: player_width,
-           audioHeight: player_height,
-           success: function(mediaElement, domObject)
-           {
-               mediaElement.addEventListener('ended', loadNextTrack, false);
-               mediaElement.addEventListener('canplay', function() {
-                    mediaPlayer.play();
-               }, false);
-                
-           }
-        });*/
-        
-        
-        /*mediaPlayer = new MediaElementPlayer("#audio-player",{
-            // if set, overrides <video width>
-            videoWidth: -1,
-            // if set, overrides <video height>
-            videoHeight: -1,
-            // width of audio player
-            audioWidth: '100%',
-            // height of audio player
-            audioHeight: 30,
-            // initial volume when the player starts
-            startVolume: 1,
-            // useful for <audio> player loops
-            loop: false,
-            // enables Flash and Silverlight to resize to content size
-            enableAutosize: true,
-            // the order of controls you want on the control bar (and other plugins below)
-            features: ['playpause','progress','current','duration','tracks','volume','fullscreen'],
-            // Hide controls when playing and mouse is not over the video
-            alwaysShowControls: false,
-            // force iPad's native controls
-            iPadUseNativeControls: false,
-            // force iPhone's native controls
-            iPhoneUseNativeControls: false, 
-            // force Android's native controls
-            AndroidUseNativeControls: false,
-            // forces the hour marker (##:00:00)
-            alwaysShowHours: false,
-            // show framecount in timecode (##:00:00:00)
-            showTimecodeFrameCount: false,
-            // used when showTimecodeFrameCount is set to true
-            framesPerSecond: 25,
-            // turns keyboard support on and off for this instance
-            enableKeyboard: true,
-            // when this player starts, it will pause other players
-            pauseOtherPlayers: true,
-            // array of keyboard commands
-            keyActions: [],
-            success: function(mediaElement, domObject)
-            {
-               mediaElement.addEventListener('ended', loadNextTrack, false);
-               mediaElement.addEventListener('canplay', function() {
-                    mediaPlayer.play();
-               }, false);
+           mediaElement.addEventListener('ended', loadNextTrack, false);
+           mediaElement.addEventListener('canplay', function() {
+                mediaPlayer.play();
+           }, false);
+       
            
-               
-            }
-        });
-        
-        mediaPlayer.play();*/
+        }
+    });
+    
+    mediaPlayer.play();*/
 
 };
 
