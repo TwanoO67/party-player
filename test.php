6c7d089ef12fddb94348d49fd1fd381b7a83cd39
Antoine WEBER <a.weber@sdv.fr>
2015-02-21 11:11:47 +0100

    test et debug

--- test.php
+++ test.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang='fr'>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+	<meta charset="UTF-8" />
+	<meta http-equiv="content-language" content="en" />
     
     <script src="/js/build/jquery.js" type="text/javascript"></script>
 	<script src="/js/build/mediaelement-and-player.min.js" type="text/javascript"></script>
 	<link rel="stylesheet" href="/js/build/mediaelementplayer.css" type="text/css">
+	<link rel="stylesheet" href="/js/build/mejs-skins.css" type="text/css">
 </head>
 
 <body>
