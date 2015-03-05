7065147c74a4241b93a48efbcb65f5322bab8440
WEBER Antoine <perso@weberantoine.fr>
2015-03-05 21:54:29 +0100

    simplification mode server

--- includes/_mode_server.php
+++ includes/_mode_server.php
@@ -39,40 +39,20 @@
 	<div class='col-xs-12 col-sm-8' id='colonne_gauche'>
 	    
 	    <div class='well placeholders'>
-	        <h2 id='main-title'>Bienvenue sur Party Player!</h2>
+	        <h2 id='main-title'>Votre JukeBox est prêt!</h2>
 	        <div id='player-wrapper' width="50%" style="width:100%;position:relative;">
-	            Votre JukeBox est maintenant en attente de musique!<br/>
-	            Demandez à vos amis d'ajouter des musiques, et elle se liront automatiquement ici!<br/>
+	            Demandez à vos amis d'ajouter des musique!<br/>
 	            <img src='/img/phone.svg' width='100px' /><br/>
-	            Pour cela, prenez votre <b>smartphone</b> : <br/>
-	            Flashez le QRCode de votre jukebox (situé à droite ==> )<br/>
+	            Flashez le QRCode situé à droite ==><br/>
 	            <br/>
 	            ou<br/>
 	            <br/>
-	            Avec votre smartphone aller sur <b>http://partyplayer.fr</b><br/>
-	            Participer à la playlist, CODE: <b><? echo $sessid ?></b><br/>
+	            Aller sur <b>http://partyplayer.fr</b><br/>
+	            Participer au JukeBox: <b><? echo $sessid ?></b><br/>
 	        </div>
 	        <div id='vote-area-ph'></div>
 	    </div>
 	    
-	    <div id='search-result' class='placeholders' style='display:none' >
-	        <h1 class="page-header" id='reserach-result-title'>Résultat de recherche</h1>
-	        <table class="table table-striped table-hover">
-	          <thead>
-	            <tr>
-	              <th>#</th>
-	              <th>Image</th>
-	              <th>Titre</th>
-	              <th>Actions</th>
-	            </tr>
-	          </thead>
-	          <tbody id='result'>
-	          </tbody>
-	        </table>
-	        
-	    </div>
-	    
-	
 	</div>
 	
 	<!-- Colonne de droite -->
@@ -81,14 +61,12 @@
 	    
 	    <div class="panel panel-warning" id='share_panel'>
 	        <div class="panel-heading">
-	            <h3 class="panel-title" style="display:inline">Partager votre playlist
+	            <h3 class="panel-title" style="display:inline">Participer à la playlist
 	            </h3>
 	            <span onclick='$("#share_panel").hide();' style='float:right;cursor:pointer;'>X</span>
 	        </div>
 	        <div class="panel-body placeholders">
-	             <!--<b><div id='username' style='display:inline;'></div></b> <span class='glyphicon glyphicon-edit' onclick='changeUsername()'></span><br/>-->
-	             Code de votre playlist : <b><?php echo $sessid ?> <br/></b>
-	             <!--<a href="<?php echo $session_url; ?>"><?php echo $session_url; ?></a><br/>-->
+	             Votre JukeBox : <b><?php echo $sessid ?> <br/></b>
 	             <img src="https://chart.googleapis.com/chart?cht=qr&chs=250x250&chl=<?php echo urlencode($session_url); ?>&choe=UTF-8" /><br/>
 	      </div>
 	    </div>
