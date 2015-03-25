b327a11a53a9c10cdf56e9b07045f41ef48e3baf
Antoine WEBER <a.weber@sdv.fr>
2015-03-25 18:38:11 +0100

    test et debug

--- index.php
+++ index.php
@@ -246,7 +246,6 @@
                     '" class="form-control input-md" /> </div>' +
                     '</div> ' +
                     '<div class="form-group"> ' +
-                    '<label class="col-md-4 control-label" for="awesomeness">Comment commencer</label> ' +
                     '<div class="col-md-4"> <div class="radio"> <label for="mode-server"> ' +
                     '<input type="radio" name="mode" id="mode-server" value="server" checked="checked"> ' +
                     'Nouveau Jukebox </label> ' +
